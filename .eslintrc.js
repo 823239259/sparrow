302623e7173db030fe75e8ae2ddceacdb0b2142f
WangDylan <Dylan.Wang@garmin.com>
2022-04-27 11:03:07 +0800

    chore: init local env

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 这里添加一行规则把这条规则隐藏
+    'import/prefer-default-export': 0,
+    'linebreak-style': [0, 'error', 'windows'],
+  },
+};
